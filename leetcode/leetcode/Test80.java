facaeac608159b503e2fbc7ef941458f214ad32d
earthgee <865603556@qq.com>
2018-05-02 19:38:59 +0800

    leetcode

---
+++ leetcode/leetcode/Test80.java
@@ -0,0 +1,22 @@
+
+//双指针
+public class Test80 {
+
+	public int removeDuplicates(int[] nums) {
+		int index=0;
+		int count=0;
+		for(int i=0;i<nums.length;i++){
+			if(i>0&&nums[i]==nums[i-1]){
+				count++;
+				if(count>=3){
+					continue;
+				}
+			}else{
+				count=1;
+			}
+			nums[index++]=nums[i];
+		}
+		return index;
+    }
+	
+}
